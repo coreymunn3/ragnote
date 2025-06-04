d495f36782cbf0c3c8aa745d699a8039e8065556
Corey Munn <michael.munn@secunetics.com>
2025-06-04 11:11:23 -0400

    setup clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import clsx from "clsx";
-
 import { Providers } from "./providers";
-
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@heroui/button";
 
 export const metadata: Metadata = {
   title: {
@@ -41,7 +47,22 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <main className="container">{children}</main>
+            <main className="container">
+              <header className="flex justify-end items-center p-4 gap-4 h-16">
+                <SignedOut>
+                  <Button color="primary">
+                    <SignInButton />
+                  </Button>
+                  <Button variant="bordered">
+                    <SignUpButton />
+                  </Button>
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </header>
+              {children}
+            </main>
           </div>
         </Providers>
       </body>
