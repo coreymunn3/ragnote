dcfab6dde503199b50de64cf02e978a34670b470
Corey Munn <michael.munn@secunetics.com>
2025-06-04 10:51:16 -0400

    initial commit with heroui, prisma, next setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@heroui/button";
+import { ThemeSwitch } from "@/components/ThemeSwitch";
+
+export default function Home() {
+  return (
+    <div>
+      <Button color="primary">Click Me</Button>
+    </div>
+  );
+}
