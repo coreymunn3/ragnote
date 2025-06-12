cd9149eb3b37a8528fb891380bd8b95fc001b62c
Corey Munn <michael.munn@secunetics.com>
2025-06-12 13:51:06 -0400

    folders can open

--- components/web/FolderItem.tsx
+++ components/web/FolderItem.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Folder } from "@/lib/types";
 import { SidebarMenuButton } from "../ui/sidebar";
-import { FolderIcon, ChevronRightIcon } from "lucide-react";
+import { ChevronDownIcon, ChevronRightIcon } from "lucide-react";
+import { useState } from "react";
 
 interface FolderItemProps {
   folder: Folder;
@@ -8,18 +10,29 @@ interface FolderItemProps {
 }
 
 const FolderItem = ({ folder, Icon }: FolderItemProps) => {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
-    <SidebarMenuButton className="flex justify-between items-center">
-      {/* Folder Name and Icon */}
-      <div className="flex items-center space-x-2">
-        {Icon}
-        <span>{`${folder.folder_name} (${folder.notes.length})`}</span>
-      </div>
-      {/* Expand Icon */}
-      <div>
-        <ChevronRightIcon className="h-4 w-4 opacity-30 hover:opacity-100" />
-      </div>
-    </SidebarMenuButton>
+    <div className={` p-1 rounded-[1.5rem]`} onClick={toggleOpen}>
+      <SidebarMenuButton className="flex justify-between items-center">
+        {/* Folder Name and Icon */}
+        <div className="flex items-center space-x-2">
+          {Icon}
+          <span>{`${folder.folder_name} (${folder.notes.length})`}</span>
+        </div>
+        {/* Expand Icon */}
+        <div>
+          <ChevronRightIcon
+            className={`h-4 w-4 transition-transform duration-200 ${open && "rotate-90"}`}
+          />
+        </div>
+      </SidebarMenuButton>
+      {open && <div className={`p-2`}>Test</div>}
+    </div>
   );
 };
 export default FolderItem;
