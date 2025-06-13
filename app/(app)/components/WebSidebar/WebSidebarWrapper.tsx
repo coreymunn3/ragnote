2dd81cc3105c3c90a64d8f3e00c449b867c77123
Corey Munn <michael.munn@secunetics.com>
2025-06-13 18:33:22 -0400

    add a global options control on web sidebar wrapper

--- app/(app)/components/WebSidebar/WebSidebarWrapper.tsx
+++ app/(app)/components/WebSidebar/WebSidebarWrapper.tsx
@@ -2,6 +2,8 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import WebSidebar from "./WebSidebar";
 import WebSidebarExternalTrigger from "./WebSidebarExternalTrigger";
 import ThemeSwitch from "@/components/ThemeSwitch";
+import { Button } from "@/components/ui/button";
+import { EllipsisVerticalIcon } from "lucide-react";
 
 export default function WebSidebarWrapper({
   children,
@@ -18,8 +20,12 @@ export default function WebSidebarWrapper({
             <WebSidebarExternalTrigger />
           </div>
           {/* right side controls */}
-          <div>
+          <div className="flex space-x-1">
             <ThemeSwitch />
+            {/* a global options control */}
+            <Button variant={"ghost"}>
+              <EllipsisVerticalIcon className="h-4 w-4" />
+            </Button>
           </div>
         </div>
         {children}
