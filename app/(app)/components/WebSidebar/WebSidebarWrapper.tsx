0bef1c1b763f3b27fa4d0f7c0301bb0451b10ff8
Corey Munn <michael.munn@secunetics.com>
2025-06-16 00:01:32 -0400

    refactor sidebar wrapper to remove the theme switch from top right and add padding to different layouts so that the Note page toolbar at the top can sit flush with the top of the page and possibly have similar toolbars in other views.

--- app/(app)/components/WebSidebar/WebSidebarWrapper.tsx
+++ app/(app)/components/WebSidebar/WebSidebarWrapper.tsx
@@ -1,9 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import WebSidebar from "./WebSidebar";
 import WebSidebarExternalTrigger from "./WebSidebarExternalTrigger";
-import ThemeSwitch from "@/components/ThemeSwitch";
-import { Button } from "@/components/ui/button";
-import { EllipsisVerticalIcon } from "lucide-react";
 
 export default function WebSidebarWrapper({
   children,
@@ -13,20 +10,9 @@ export default function WebSidebarWrapper({
   return (
     <SidebarProvider>
       <WebSidebar />
-      <main className="w-full p-1">
-        <div className="flex items-center">
-          {/* left side controls */}
-          <div className="flex-1">
-            <WebSidebarExternalTrigger />
-          </div>
-          {/* right side controls */}
-          <div className="flex space-x-1">
-            <ThemeSwitch />
-            {/* a global options control */}
-            <Button variant={"ghost"}>
-              <EllipsisVerticalIcon className="h-4 w-4" />
-            </Button>
-          </div>
+      <main className="w-full relative">
+        <div className="absolute top-0 left-0 p-2">
+          <WebSidebarExternalTrigger />
         </div>
         {children}
       </main>
