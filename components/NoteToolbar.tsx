51826ac1c4529910e39e444f73f5887d7e087613
Corey Munn <michael.munn@secunetics.com>
2025-07-13 18:50:01 -0400

    update skeleton in NoteToolbar

--- components/NoteToolbar.tsx
+++ components/NoteToolbar.tsx
@@ -15,6 +15,7 @@ import OptionsMenu from "./OptionsMenu";
 import { useNoteVersion } from "@/contexts/NoteVersionContext";
 import { DateTime } from "luxon";
 import { useUpdateNote } from "@/hooks/note/useUpdateNote";
+import { Skeleton } from "./ui/skeleton";
 
 const NoteToolbar = () => {
   const { id } = useParams();
@@ -32,11 +33,11 @@ const NoteToolbar = () => {
     return (
       <div className="flex items-center justify-between px-14 py-2">
         <div className="flex items-end space-x-2">
-          <div className="h-6 w-32 bg-gray-200 animate-pulse rounded"></div>
-          <div className="h-6 w-16 bg-gray-200 animate-pulse rounded"></div>
+          <Skeleton className="h-8 w-32" />
+          <Skeleton className="h-8 w-16" />
         </div>
         <div className="flex items-center space-x-2">
-          <div className="h-4 w-24 bg-gray-200 animate-pulse rounded"></div>
+          <Skeleton className="h-8 w-32" />
         </div>
       </div>
     );
