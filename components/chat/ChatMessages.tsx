dd34a55fe165de609437bf4936a6d2602ab08575
Corey Munn <michael.munn@secunetics.com>
2025-08-18 10:43:32 -0400

    Implement scroll to bottom of conversation when new messsages are sent

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import { ChatDisplayMessage } from "@/lib/types/chatTypes";
 import { AnimatedListItem } from "@/components/animations";
 import ChatMessage from "./ChatMessage";
@@ -9,8 +10,23 @@ interface ChatMessagesProps {
 }
 
 const ChatMessages = ({ messages }: ChatMessagesProps) => {
+  const messagesEnd = useRef<null | HTMLDivElement>(null);
+  console.log(messagesEnd);
+
+  const scrollToBottom = () => {
+    if (messagesEnd.current) {
+      messagesEnd.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  // scroll to the bottom when we add more messages
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
     <div className="flex flex-col space-y-2 overflow-y-auto">
+      {/* Messages in the conversation */}
       {messages.map((message, index) => (
         <AnimatedListItem
           key={message.id}
@@ -23,6 +39,8 @@ const ChatMessages = ({ messages }: ChatMessagesProps) => {
           <ChatMessage message={message} />
         </AnimatedListItem>
       ))}
+      {/* Messages end - used to scroll into view */}
+      <div ref={messagesEnd}></div>
     </div>
   );
 };
