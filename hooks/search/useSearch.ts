aee3304f02e6cd9a8d5b4237705f30ec0114addf
Corey Munn <michael.munn@secunetics.com>
2025-10-06 11:50:09 -0400

    improve the useSearch hook to type out options and response, and provide an error notification

--- hooks/search/useSearch.ts
+++ hooks/search/useSearch.ts
@@ -1,13 +1,28 @@
+import { handleClientSideMutationError } from "@/lib/errors/handleClientSideMutationError";
+import { SearchResultNote } from "@/lib/types/aiTypes";
+import { UseMutationHookOptions } from "@/lib/types/sharedTypes";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
-const executeSearch = async (query: string): Promise<unknown> => {
+const executeSearch = async (query: string): Promise<SearchResultNote[]> => {
   const res = await axios.get(`/api/search?query=${encodeURIComponent(query)}`);
   return res.data;
 };
 
-export function useSearch() {
+export type useSearchOptions = UseMutationHookOptions<
+  SearchResultNote[],
+  Error,
+  string
+>;
+
+export function useSearch(options?: useSearchOptions) {
   return useMutation({
+    ...options,
     mutationFn: executeSearch,
+    onError: (error, variables, context) => {
+      handleClientSideMutationError(error, "Failed to search");
+      // custom onError callback
+      options?.onError?.(error, variables, context);
+    },
   });
 }
