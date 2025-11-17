41eac81aef44e83b0efc857ffbab82b6780f05eb
Corey Munn <michael.munn@secunetics.com>
2025-11-17 10:59:50 -0500

    simplify getDbUser

--- lib/getDbUser.ts
+++ lib/getDbUser.ts
@@ -21,54 +21,6 @@ export async function getDbUser(safeMode = false) {
       },
     }),
   });
-  /**
-   * Race condition: user exists in Clerk but not in our DB yet
-   * This can happen if user navigates to app before webhook processes
-   * not likely to encounter this!
-   */
-  if (!dbUser) {
-    console.log(
-      `User ${clerkUserId} not found in DB - creating from Clerk data`
-    );
-    const cc = await clerkClient();
-
-    try {
-      // Get user data from Clerk to create the database record
-
-      const clerkUser = await cc.users.getUser(clerkUserId!);
-      const clerkService = new ClerkService();
-
-      // Create user using the same logic as the webhook
-      await clerkService.createUserFromClerk({
-        clerkId: clerkUserId!,
-        email: clerkUser.emailAddresses[0]?.emailAddress || "",
-        username: clerkUser.username,
-        firstName: clerkUser.firstName,
-        lastName: clerkUser.lastName,
-        avatarUrl: clerkUser.imageUrl,
-      });
-
-      // Now fetch the newly created user
-      dbUser = await prisma.app_user.findFirst({
-        where: {
-          clerk_id: clerkUserId!,
-        },
-        ...(safeMode && {
-          select: {
-            username: true,
-            avatar_url: true,
-          },
-        }),
-      });
-
-      console.log(`Successfully created DB user for ${clerkUserId}`);
-    } catch (error) {
-      console.error(`Failed to create DB user for ${clerkUserId}:`, error);
-      throw new Error(
-        `Unable to find or create database user with Clerk Id: ${clerkUserId}`
-      );
-    }
-  }
 
   if (!dbUser) {
     throw new Error(
