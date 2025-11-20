25e06cf151e0581ba12181134d48b113a6ae6b2d
Corey Munn <michael.munn@secunetics.com>
2025-11-20 11:19:56 -0500

    mobile background color

--- app/(app)/components/layouts/mobile/layout.tsx
+++ app/(app)/components/layouts/mobile/layout.tsx
@@ -6,7 +6,7 @@ export default function MobileLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-sidebar">
       <MobileHeader />
       {children}
     </div>
