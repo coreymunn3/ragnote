c974363b98b668adcb9b7b66e63c0d4534efa4f5
Corey Munn <michael.munn@secunetics.com>
2025-11-28 13:56:51 -0500

    ensure not too much padding at top of all mobile layouts to accomodate correct mobile header view

--- app/(app)/components/layouts/mobile/note/layout.tsx
+++ app/(app)/components/layouts/mobile/note/layout.tsx
@@ -1,4 +1,4 @@
 const MobileNoteLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="px-2 pt-16 mx-auto bg-background"> {children}</div>;
+  return <div className="px-2 pt-14 mx-auto bg-background"> {children}</div>;
 };
 export default MobileNoteLayout;
