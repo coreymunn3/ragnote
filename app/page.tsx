93c8a3080df5b0162d57f18b7ee618421d608304
Corey Munn <michael.munn@secunetics.com>
2025-12-20 16:06:07 -0500

    add the subtle grid background to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -17,16 +17,21 @@ export default async function Home() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <LandingNavbar />
-      <main className="flex-1">
-        <HeroSection />
-        <AIChatDemo />
-        <FeaturesSection />
-        <HowItWorksSection />
-        <PricingSection />
-      </main>
-      <LandingFooter />
+    <div className="min-h-screen flex flex-col relative">
+      {/* Global grid pattern for landing page */}
+      <div className="fixed inset-0 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:4rem_4rem] pointer-events-none z-0" />
+
+      <div className="relative z-10 flex flex-col flex-1">
+        <LandingNavbar />
+        <main className="flex-1">
+          <HeroSection />
+          <AIChatDemo />
+          <FeaturesSection />
+          <HowItWorksSection />
+          <PricingSection />
+        </main>
+        <LandingFooter />
+      </div>
     </div>
   );
 }
