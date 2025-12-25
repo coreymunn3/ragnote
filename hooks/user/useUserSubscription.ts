9046ebfef48d781ecf854ff2915e28a2b950bd12
Corey Munn <michael.munn@secunetics.com>
2025-12-25 14:39:55 -0500

    enhance frontend pro access guard in the useUserSub hook

--- hooks/user/useUserSubscription.ts
+++ hooks/user/useUserSubscription.ts
@@ -2,6 +2,7 @@ import { UseQueryHookOptions } from "@/lib/types/sharedTypes";
 import { UserSubscription } from "@/lib/types/userTypes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { DateTime } from "luxon";
 
 const getUserSubscription = async (): Promise<UserSubscription> => {
   const res = await axios.get("/api/user/subscription");
@@ -19,8 +20,28 @@ export const useUserSubscription = (
     queryFn: getUserSubscription,
     ...options,
   });
+
+  const hasProAccess = (): boolean => {
+    if (!subscriptionQuery.isSuccess || !subscriptionQuery.data) {
+      return false;
+    }
+    const { tier, end_date } = subscriptionQuery.data;
+    // if not pro, then false
+    if (tier !== "PRO") {
+      return false;
+    }
+    // if no end date, then the user is free tier
+    if (!end_date) {
+      return false;
+    }
+    // if end date is not in the future, then it has expired
+    const endDateTime = DateTime.fromISO(end_date);
+    const now = DateTime.now();
+    return endDateTime > now;
+  };
+
   return {
     ...subscriptionQuery,
-    isPro: subscriptionQuery.isSuccess && subscriptionQuery.data.tier === "PRO",
+    isPro: hasProAccess(),
   };
 };
