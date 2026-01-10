0bf0a1dd9eccb174974d2c864986349d10d58de1
Corey Munn <michael.munn@secunetics.com>
2026-01-10 13:47:00 -0500

    remove random auth check in a layout file that shouldn't be there

--- app/(app)/note/[id]/layout.tsx
+++ app/(app)/note/[id]/layout.tsx
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
 import WebNoteLayout from "@/app/(app)/components/layouts/web/note/layout";
 import MobileNoteLayout from "@/app/(app)/components/layouts/mobile/note/layout";
 import ResponsiveLayout from "@/components/ResponsiveLayout";
@@ -9,13 +7,6 @@ export default async function LayoutSwitch({
 }: {
   children: React.ReactNode;
 }) {
-  const { userId } = await auth();
-
-  // Protect this page from non-logged-in users
-  if (!userId) {
-    redirect("/");
-  }
-
   return (
     <ResponsiveLayout MobileLayout={MobileNoteLayout} WebLayout={WebNoteLayout}>
       {children}
