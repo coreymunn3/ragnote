f941f0bbf4974c76936b00bef8586aa3c5eddd74
Corey Munn <michael.munn@secunetics.com>
2026-01-10 13:46:05 -0500

    fix dbUser auth pattern in recently deleted

--- app/(app)/recently-deleted/page.tsx
+++ app/(app)/recently-deleted/page.tsx
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
 import WebRecentlyDeletedContent from "../components/RecentlyDeleted/WebRecentlyDeletedContent";
 import MobileRecentlyDeletedContent from "../components/RecentlyDeleted/MobileRecentlyDeletedContent";
 import ResponsivePage from "@/components/ResponsivePage";
@@ -7,17 +5,9 @@ import { getDbUser } from "@/lib/getDbUser";
 import { DeletedItemsService } from "@/services/deleted/deletedItemsService";
 
 export default async function RecentlyDeletedPage() {
-  const { userId } = await auth();
   const deletedItemsService = new DeletedItemsService();
-
-  // Protect this page from non-logged-in users
-  if (!userId) {
-    redirect("/");
-  }
-
   // get the database user
   const dbUser = await getDbUser();
-
   // get the deleted items
   let deletedItems;
   try {
