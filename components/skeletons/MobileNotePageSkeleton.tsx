2b3ea152a68cdff7d7d4bf584c643702be36386c
Corey Munn <michael.munn@secunetics.com>
2026-01-14 12:05:06 -0500

    toolbark skeleton heights

--- components/skeletons/MobileNotePageSkeleton.tsx
+++ components/skeletons/MobileNotePageSkeleton.tsx
@@ -8,11 +8,11 @@ const MobileNotePageSkeleton = () => {
       <div className="flex items-center justify-between px-4 py-2 border-b">
         <div className="flex items-center space-x-2">
           <Skeleton className="h-6 w-16 rounded-md" /> {/* Version */}
-          <Skeleton className="h-4 w-12 rounded-full" /> {/* Status */}
+          <Skeleton className="h-6 w-12 rounded-full" /> {/* Status */}
         </div>
         <div className="flex items-center space-x-2">
-          <Skeleton className="h-8 w-8 rounded-full" /> {/* Chat */}
-          <Skeleton className="h-8 w-8 rounded-full" /> {/* Publish */}
+          <Skeleton className="h-6 w-8 rounded-full" /> {/* Chat */}
+          <Skeleton className="h-6 w-8 rounded-full" /> {/* Publish */}
         </div>
       </div>
 
