442ccd03fdaf6a673e45381f90669166d463658d
Corey Munn <coreymunn3@gmail.com>
2026-04-17 12:30:50 -0400

    realism improvements to mobile note page skeleton

--- components/skeletons/mobile/MobileNotePageSkeleton.tsx
+++ components/skeletons/mobile/MobileNotePageSkeleton.tsx
@@ -3,14 +3,14 @@ import EditorSkeleton from "../EditorSkeleton";
 
 const MobileNotePageSkeleton = () => {
   return (
-    <div className="flex flex-col h-[calc(100vh-4rem)] pt-4">
+    <div className="flex flex-col h-[calc(100vh-4rem)]">
       {/* Toolbar Skeleton */}
       <div className="flex items-center justify-between px-4 py-2">
         <div className="flex items-center space-x-2">
           <Skeleton className="h-6 w-16 rounded-md" /> {/* Version */}
-          <Skeleton className="h-6 w-12 rounded-full" /> {/* Status */}
         </div>
         <div className="flex items-center space-x-2">
+          <Skeleton className="h-6 w-8 rounded-full" /> {/* Copy */}
           <Skeleton className="h-6 w-8 rounded-full" /> {/* Chat */}
           <Skeleton className="h-6 w-8 rounded-full" /> {/* Publish */}
         </div>
